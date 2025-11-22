06fbd032e4a92229aae47773ba9cc293487769b6
kumarsmanmohan83 <kumarsmanmohan83@gmail.com>
2025-11-22 21:31:34 +0530

    main activity setup and issue fixed

--- app/src/main/java/com/example/moduleguard/data/repository/MockRepository.kt
+++ app/src/main/java/com/example/moduleguard/data/repository/MockRepository.kt
@@ -1,16 +1,23 @@
 package com.example.moduleguard.data.repository
 
 import android.content.Context
+import com.example.moduleguard.R
 import com.example.moduleguard.data.models.MockResponse
-import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import kotlinx.coroutines.delay
 
 class MockRepository(private val context: Context) {
+
     suspend fun loadMockData(): MockResponse {
-        // simulate small delay to mimic fetching
         delay(200)
-        val text = context.assets.open("mock_data.json").bufferedReader().use { it.readText() }
-        return Json { ignoreUnknownKeys = true }.decodeFromString(text)
+
+        val text = context.assets.open("mock_data.json")
+            .bufferedReader()
+            .use { it.readText() }
+
+        return Json {
+            ignoreUnknownKeys = true
+        }.decodeFromString<MockResponse>(text)
     }
 }
+
--- app/src/main/java/com/example/moduleguard/ui/MainActivity.kt
+++ app/src/main/java/com/example/moduleguard/ui/MainActivity.kt
@@ -11,37 +11,23 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.moduleguard.data.repository.MockRepository
+import com.example.moduleguard.ui.screen.ModuleAccessScreen
 import com.example.moduleguard.ui.theme.ModuleGuardTheme
+import com.example.moduleguard.viewmodel.ModuleViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val repo = MockRepository(applicationContext)
+        val vm = ModuleViewModel(repo)
+        vm.load()
+
         setContent {
             ModuleGuardTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                ModuleAccessScreen(vm)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ModuleGuardTheme {
-        Greeting("Android")
     }
 }
